fe8bd18b92bc8a9f2a0e599d5e621a2024c6231a
BoungbaiComputers <justlyjohn198@gmail.com>
2021-10-10 22:54:35 +0100

    Users can log in

--- routes/auth.js
+++ routes/auth.js
@@ -10,12 +10,12 @@ const { User } = require("../models/User");
 // Extract router from express
 const router = express.Router();
 
-// GET - Display Sign-Up Form
+// GET - Display User Sign-Up Form
 router.get("/sign-up", (req, res) => {
   res.render("sign-up");
 });
 
-// POST - Submit Sign-Up Data
+// POST - Create User
 router.post("/sign-up", (req, res) => {
   const user = new User(req.body);
 
@@ -40,7 +40,45 @@ router.post("/sign-up", (req, res) => {
     });
 });
 
-// GET - Logout users
+// GET - Display User Login Form
+router.get("/log-in", (req, res) => {
+  res.render("log-in");
+});
+
+// POST - Login user
+router.post("/log-in", (req, res) => {
+  const { username, password } = req.body;
+
+  User.findOne({ username }, "username password")
+    .then((user) => {
+      if (!user) {
+        return res
+          .status(401)
+          .send({ message: "Username or password incorrect!" });
+      }
+
+      bcrypt.compare(password, user.password, (err, isMatch) => {
+        if (!isMatch) {
+          return res
+            .status(401)
+            .send({ message: "Username or password incorrect!" });
+        }
+      });
+
+      const token = jwt.sign(
+        { _id: user._id, username: user.username },
+        process.env.SECRET_KEY
+      );
+
+      res.cookie("nToken", token, { maxAge: 900000, httpOnly: true });
+    })
+    .catch((err) => {
+      console.log(err.message);
+      x;
+    });
+});
+
+// GET - Logout User
 router.get("/logout", (req, res) => {
   res.clearCookie("nToken");
   res.redirect("/");
